87a82b32439f6dd7fbfbb845836889099acb1fe7
muthu-PM <muthu@abc.com>
2025-01-31 15:39:48 +0530

    uploaded v1

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const productRouter = require('./Routes/ProductRoutes')
 app.use(express.json())
 app.use("", productRouter);
 
+
 app.listen('5000', ()=> console.log('server running on 5000'))
 
 mongoose.connect('mongodb://localhost:27017/sample')
